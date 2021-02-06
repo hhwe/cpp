f49e18049b0b375983330b458a5b14077671fb1b
hhwe <hhw12125@gmail.com>
2021-02-06 20:11:04 +0800

    tmp

--- helloworld.cc
+++ helloworld.cc
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+// class A
+// {
+// public:
+//     A() {}
+//     void destory() { delete this; }
+
+// private:
+//     ~A() {}
+// };
+
+// class B  
+// {  
+// private:  
+//     void* operator new(size_t t){}     // 注意函数的第一个参数和返回值都是固定的  
+//     void operator delete(void* ptr){} // 重载了new就需要重载delete  
+// public:  
+//     B(){}  
+//     ~B(){}  
+// };
+
+
 int main()
 {
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
@@ -13,4 +34,11 @@ int main()
         cout << word << " ";
     }
     cout << endl;
+
+
+
+    // A a;
+    // A *a  = new A;
+    // B b;
+    // B *b = new B;
 }
--- primer/ex13_39.cc
+++ primer/ex13_39.cc
@@ -1,5 +1,25 @@
 #include "common.h"
 
+class String
+{
+private:
+    string *elements;
+    string *first_string;
+    allocator<string> alloc;
+
+public:
+    String(/* args */);
+    ~String();
+};
+
+String::String(/* args */)
+{
+}
+
+String::~String()
+{
+}
+
 class StrVec
 {
 private:
@@ -236,8 +256,55 @@ void runQueries(ifstream &infile)
     }
 }
 
+void foo(String x)
+{
+    std::cout << x.c_str() << std::endl;
+}
+
+void bar(const String &x)
+{
+    std::cout << x.c_str() << std::endl;
+}
+
+String baz()
+{
+    String ret("world");
+    return ret;
+}
+
 int main()
 {
+    char text[] = "world";
+
+    String s0;
+    String s1("hello");
+    String s2(s0);
+    String s3 = s1;
+    String s4(text);
+    s2 = s1;
+
+    foo(s1);
+    bar(s1);
+    foo("temporary");
+    bar("temporary");
+    String s5 = baz();
+
+    std::vector<String> svec;
+    svec.reserve(8);
+    svec.push_back(s0);
+    svec.push_back(s1);
+    svec.push_back(s2);
+    svec.push_back(s3);
+    svec.push_back(s4);
+    svec.push_back(s5);
+    svec.push_back(baz());
+    svec.push_back("good job");
+
+    for (const auto &s : svec)
+    {
+        std::cout << s.c_str() << std::endl;
+    }
+
     ifstream file("data/storyDataFile.txt");
     runQueries(file);
 }
